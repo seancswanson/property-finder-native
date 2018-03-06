e67c88f45503e637d40f3f662ad86e16a92f6bd0
seanthaswan <sean-swan@hotmail.com>
2018-03-05 16:29:03 -0800

    Removed placeholder app text

--- App.js
+++ App.js
@@ -25,13 +25,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+          Hello, world!
         </Text>
       </View>
     );
